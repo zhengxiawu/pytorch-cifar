d9373bbf3414a9f4e835f4171c294096073c45fa
zhengxiawu <zhengxiawu@126.com>
2020-05-28 09:59:54 +0800

    time delte

--- log_reader.py
+++ log_reader.py
@@ -2,39 +2,7 @@
 read the result from the log
 '''
 import os
-
-
-def get_folder_list(walk_dir):
-    folder_list = []
-    walk_dir = os.path.abspath(walk_dir)
-    method_list = os.listdir(walk_dir)
-    print(method_list)
-    # for method in method_list:
-    #     model_list = os.listdir(os.path.join(walk_dir, method))
-    #     if len(method_list) > 0:
-    #         for model in model_list:
-    #             dataset_list = os.listdir(
-    #                 os.path.join(walk_dir, method, model))
-    #             for dataset in dataset_list:
-    #                 train_name_list = os.listdir(
-    #                     os.path.join(walk_dir, method, model, dataset))
-    #                 for train_name in train_name_list:
-    #                     this_train_folder = os.path.join(
-    #                         walk_dir, method, model, dataset, train_name)
-    #                     folder_list.append(this_train_folder)
-    return folder_list
-
-
-def read_result(walk_dir):
-    folder_list = get_folder_list(walk_dir)
-    for this_train_folder in folder_list:
-        log_file = os.path.join(this_train_folder, 'logger.log')
-        with open(log_file) as f:
-            log_lines = f.readlines()
-        if 'Final' in log_lines[-1]:
-            if 'dali' in this_train_folder.split('/')[-1]:
-                print(this_train_folder)
-                print(log_lines[-1])
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -47,6 +15,10 @@ if __name__ == "__main__":
             if os.path.isfile(model_log_path):
                 with open(model_log_path) as f:
                     log_lines = f.readlines()
+                start_time = datetime.strptime(log_lines[0][0:17], '%m/%d %I:%M:%S %p')
+                end_time = datetime.strptime(log_lines[-1][0:17], '%m/%d %I:%M:%S %p')
+                training_hours = (start_time - end_time).total_seconds() / 3600.
+                print("Training times is {0}".format(training_hours))
                 _temp = []
                 for line in log_lines:
                     if 'parameter size' in line:
